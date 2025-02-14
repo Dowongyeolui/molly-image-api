4a1418e4bef2d0cc8e22b935ad5a3bd5646e4582
LeeJeYoun <dlwpdbs1229@naver.com>
2025-02-14 14:53:49 +0900

    first commit

---
+++ src/main/java/org/hidevelop/mollyapiapi/MollyApiApiApplication.java
@@ -0,0 +1,13 @@
+package org.hidevelop.mollyapiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MollyApiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MollyApiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hidevelop/mollyapiapi/MollyApiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hidevelop.mollyapiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MollyApiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
